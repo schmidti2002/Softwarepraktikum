fd3dc19abc49abc12d7d062085f3c5506c09486e
schmidti2002 <florian-hartha@web.de>
2024-01-08 14:07:57 +0100

    Erstellen der Resource der User API Methoden

--- Meilensteine/Meilenstein_04/user_login.py
+++ Meilensteine/Meilenstein_04/user_login.py
@@ -1,4 +1,5 @@
 # alle
+from typing import Any
 from flask import Flask, request
 from flask_restful import Api, Resource, abort
 from flask_cors import CORS
@@ -41,3 +42,155 @@ class login(Resource):
             response = make_response('Login erfolgreich')
             response.set_cookie('sessiontoken', token)  # Cookie setzen, um den Session-Token zu speichern
             return response
+        
+
+class user(Resource):
+    def get(self):
+        cursor = database.cursor()
+
+        # Daten aus dem Request holen und überprüfen
+        try:
+            user_token = request.cookies.get('sessiontoken')
+            cursor.execute("SELECT * FROM User WHERE session_token = %s", (user_token))
+            result = cursor.fetchall()
+            if len(result) == 0:
+                return abort(401, message="API key is missing or invalid")
+        except:
+            return abort(401, message="API key is missing or invalid ")
+        
+        # SQL-Abfrage und Ergebnis zurückgeben
+        cursor.execute("SELECT `username`, `rights`, `email` FROM User WHERE session_token = %s", (user_token))
+        result = cursor.fetchall()
+
+        response_dic = {"username": result[0][0], "admin": result[0][1], "email": result[0][2]}
+        return (200,response_dic)
+    
+    def post(self):
+        cursor = database.cursor()
+
+        # Daten aus dem Request holen und überprüfen
+        try:
+            user_token = request.cookies.get('sessiontoken')
+            cursor.execute("SELECT `rights` FROM User WHERE session_token = %s", (user_token))
+            result = cursor.fetchall()
+            if len(result) == 0 or result[0][0] != 1:
+                return abort(401, message="API key is missing or invalid")
+        except:
+            return abort(401, message="API key is missing or invalid ")
+        
+        # Daten aus dem Request holen
+        username = request.form.get("username")
+        password = request.form.get("password")
+        email = request.form.get("email")
+        admin = request.form.get("admin")
+
+        # SQL-Abfrage
+        cursor.execute("INSERT INTO User (username, password, email, rights) VALUES (%s, %s, %s, %s)", (username, sha256(password.encode('utf-8')).hexdigest(), email, admin))
+        database.commit()
+        return (200,"user successfully created")
+    
+    def put(self):
+        cursor = database.cursor()
+
+        # Daten aus dem Request holen und überprüfen
+        try:
+            user_token = request.cookies.get('sessiontoken')
+            cursor.execute("SELECT `rights` FROM User WHERE session_token = %s", (user_token))
+            result = cursor.fetchall()
+            if len(result) == 0:
+                return abort(404, message="User not found ")
+            if result[0][0] != 1:
+                return abort(403, message="User not allowed to execute this operation")
+        except:
+            return abort(401, message="API key is missing or invalid ")
+        
+        # Daten aus dem Request holen
+        username = request.form.get("username")
+        password = request.form.get("password")
+        email = request.form.get("email")
+        admin = request.form.get("admin")
+
+        # SQL-Abfrage
+        cursor.execute("UPDATE User SET password = %s, email = %s, rights = %s WHERE username = %s", (sha256(password.encode('utf-8')).hexdigest(), email, admin, username))
+        database.commit()
+        return (200,"user got updated")
+    
+class user_delete(Resource):
+    def delete(self, edit_userid):
+        cursor = database.cursor()
+
+        # Daten aus dem Request holen und überprüfen
+        try:
+            user_token = request.cookies.get('sessiontoken')
+            cursor.execute("SELECT `rights` FROM User WHERE session_token = %s", (user_token))
+            result = cursor.fetchall()
+            if len(result) == 0:
+                return abort(401, message="API key is missing or invalid ")
+            if result[0][0] != 1:
+                return abort(403, message="User not allowed to execute this operation")
+        except:
+            return abort(401, message="API key is missing or invalid ")
+        
+        # Daten des zu löschenden Users überprüfen
+        cursor.execute("SELECT * FROM User WHERE user_id = %s", (edit_userid,))
+        if len(cursor.fetchall()) == 0:
+            return abort(404, message="User not found ")
+
+        # SQL-Abfrage
+        cursor.execute("DELETE FROM User WHERE user_id = %s", (edit_userid,))
+        database.commit()
+        return (200,"user got deleted")
+    
+    def get(self, edit_userid):
+        cursor = database.cursor()
+
+        # Daten aus dem Request holen und überprüfen
+        try:
+            user_token = request.cookies.get('sessiontoken')
+            cursor.execute("SELECT `rights` FROM User WHERE session_token = %s", (user_token))
+            result = cursor.fetchall()
+            if len(result) == 0:
+                return abort(401, message="API key is missing or invalid ")
+            if result[0][0] != 1:
+                return abort(403, message="User not allowed to execute this operation")
+        except:
+            return abort(401, message="API key is missing or invalid ")
+        
+        # Daten des Users überprüfen
+        cursor.execute("SELECT * FROM User WHERE user_id = %s", (edit_userid,))
+        result = cursor.fetchall()
+        if len(result) == 0:
+            return abort(404, message="User not found ")
+
+        # SQL-Abfrage
+        cursor.execute("SELECT `user_is`, `username`, `rights`, `email` FROM User WHERE user_id = %s", (edit_userid,))
+        result = cursor.fetchall()
+
+        response_dic = {"id":result[0][0],"username": result[0][1], "admin": result[0][2], "email": result[0][3]}
+        return (200,response_dic)
+    
+class useres(Resource):
+    def get(self):
+        cursor = database.cursor()
+
+        # Daten aus dem Request holen und überprüfen
+        try:
+            user_token = request.cookies.get('sessiontoken')
+            cursor.execute("SELECT `rights` FROM User WHERE session_token = %s", (user_token))
+            result = cursor.fetchall()
+            if len(result) == 0:
+                return abort(401, message="API key is missing or invalid ")
+            if result[0][0] != 1:
+                return abort(403, message="User not allowed to execute this operation")
+        except:
+            return abort(401, message="API key is missing or invalid ")
+        
+        # SQL-Abfrage
+        cursor.execute("SELECT `user_id`, `username`, `rights`, `email` FROM User")
+        result = cursor.fetchall()
+
+        response_dic = []
+        for i in range(len(result)):
+            response_dic.append({"id":result[i][0],"username": result[i][1], "admin": result[i][2], "email": result[i][3]})
+        return (200,response_dic)
+     
