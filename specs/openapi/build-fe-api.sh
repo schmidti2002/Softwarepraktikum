1735f4cf4f208d3bf59d6b169009ea19a1917caa
De_Coder <de.coder@gmx.de>
2024-01-11 15:41:54 +0100

    move api build into frontend

--- specs/openapi/build-fe-api.sh
+++
@@ -1,9 +0,0 @@
-#source build-fe-api.sh
-export PATH="$(yarn global bin):$PATH"
-rm -rf ./out/ts
-mkdir -p ./out/ts
-cp ./out/ts-build-files/* ./out/ts/
-cd ./out/ts
-yarn install
-yarn run build
-cd ../..
