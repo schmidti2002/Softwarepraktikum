e1f464fc8c1dbebc43515ecd0a41af29b973152a
thori2t <thoralf.teumer@web.de>
2024-02-17 11:48:01 +0100

    Tests für BubbleSortLogic

--- Meilensteine/Meilenstein_04/frontend/test/BubbleSortLogic.test.js
+++ Meilensteine/Meilenstein_04/frontend/test/BubbleSortLogic.test.js
@@ -1,8 +1,84 @@
-// Jest Beispielfunktion
+import fs from 'fs';
+import path from 'path';
 import {
-  expect, it,
+  expect, describe, test, beforeEach,
 } from '@jest/globals';
+import BubbleSort from '../src/BubbleSortLogic';
 
-it('adds 1 + 2 to equal 3', () => {
-  expect(1 + 2).toBe(3);
+const bubblesortHtml = fs.readFileSync(path.resolve(__dirname, '../src/BubbleSort.html'), 'utf8');
+
+describe('BubbleSort', () => {
+  describe('helper functions', () => {
+    // "Dumme" Nutzereingaben
+    let bubble;
+    beforeEach(() => {
+      // Set up our document body
+      document.body.innerHTML = bubblesortHtml;
+      bubble = new BubbleSort();
+    });
+
+    //GenerateRandomNumbers() Test
+    test.each([
+      [0, 0],
+      [-1, 0],
+      [0.5, 0],
+      ['einString', 0],
+      ['a', 0],
+      ['42', 0],
+      [10000, 0],
+      [Number.MIN_SAFE_INTEGER, 0],
+      [true, 0],
+      [false, 0],
+      [1, 1],
+      [42, 42],
+      ['1,2,3,4,5', 0],
+      ['a,b,c', 0],
+      ['1,b,3,d', 0]])('generateRandomNumbers() & generate(%s)', (input, solution) => {
+      // solution = länge des Arrays, das erstellt werden soll
+      // Ich habe einfach mal 0 für falsche eingaben Festgelegt
+      bubble.generateRandomNumbers(input);
+      expect(bubble.exec.state.vars.arr.length).toBe(solution);
+      bubble.exec.state.vars.arr.forEach((element) => {
+        expect(Number.isInteger(element)).toBeTruthy();
+        expect(element).toBeGreaterThanOrEqual(0);
+      });
+    });
+
+    //editArray() Test
+    test.each([
+      [[1], [1]],
+      [[true], []],
+      [false, []],
+      [[1.2, 7.4], []],
+      [[1, 2, 3, 4.5], []],
+      [[1, 2, 3, 4], [1, 2, 3, 4]],
+      [10, []],
+      [[1], [1]],
+      [[42, 1000], []],
+      [[50, 10, 3, 20], [50, 10, 3, 20]],
+      [['string', 'noch einer'], []],
+      [[-1], []]])('editArray()', (input, solution) => {
+      bubble.editArray(input);
+      expect(bubble.exec.state.vars.arr).toBe(solution);
+      bubble.exec.state.vars.arr.forEach((element) => {
+        expect(Number.isInteger(element)).toBeTruthy();
+        expect(element).toBeGreaterThanOrEqual(0);
+      });
+    });
+
+    //showOutput() Test
+    //////////////////////////////////////////////////////////////////////////////////
+    //Ich verstehe das #stateChangeCallback leider nicht
+    test.each(
+      ['input', 'solution'],
+      ['input','solution'])('showOutput', (input, solution) => {
+      document.getElementById('userInput').value = input;
+      bubble.showOutput();
+      expect(bubble.exec.state.vars.arr.length).toBe(solution);
+      bubble.exec.state.vars.arr.forEach((element) => {
+        expect(Number.isInteger(element)).toBeTruthy();
+        expect(element).toBeGreaterThanOrEqual(0);
+      });
+    });
+  });
 });
