b6b7e1f099384f49fc595e433d2745a38a990c05
schmidti2002 <florian-hartha@web.de>
2024-02-19 20:18:25 +0100

    Update der EasterEgg Tests

--- Meilensteine/Meilenstein_04/easteregg.py
+++ Meilensteine/Meilenstein_04/easteregg.py
@@ -1,9 +1,13 @@
 # alle
-from flask_restful import Resource
-from flask import jsonify
+from flask_restful import Resource, Api
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+api = Api(app)
+
 
 class brew_coffee(Resource):
     def get(self):
         return jsonify("Coffee is brewing", 418)
 
-
+app.add_resource(brew_coffee, '/brew_coffee')
