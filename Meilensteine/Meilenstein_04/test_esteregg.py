a360084ce90f30a98f5650d293a83d83156f48c9
schmidti2002 <florian-hartha@web.de>
2024-02-19 20:25:40 +0100

    modified:   Meilensteine/Meilenstein_04/test_esteregg.py

--- Meilensteine/Meilenstein_04/test_esteregg.py
+++ Meilensteine/Meilenstein_04/test_esteregg.py
@@ -6,8 +6,8 @@ def client():
     app.testing = True
     with app.test_client() as client:
         yield client
-        
-def test_esteregg():
+
+def test_esteregg(client):
     response = client.get('/brew_coffee')
     assert response.status_code == 418
     assert b'Coffee is brewing' in response.data
