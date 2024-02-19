1cf0989a94f4edbd9fa9fb7d349c4b7551315f5f
schmidti2002 <florian-hartha@web.de>
2024-02-19 20:20:49 +0100

    Fehler behoben 	modified:   Meilensteine/Meilenstein_04/easteregg.py

--- Meilensteine/Meilenstein_04/easteregg.py
+++ Meilensteine/Meilenstein_04/easteregg.py
@@ -10,4 +10,4 @@ class brew_coffee(Resource):
     def get(self):
         return jsonify("Coffee is brewing", 418)
 
-app.add_resource(brew_coffee, '/brew_coffee')
+api.add_resource(brew_coffee, '/brew_coffee')
