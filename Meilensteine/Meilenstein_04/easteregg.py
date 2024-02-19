3f647ed8931c1f56a8c4b440f87210a32bb3fbf7
schmidti2002 <florian-hartha@web.de>
2024-02-19 20:47:45 +0100

    Test von Flask_wft

--- Meilensteine/Meilenstein_04/easteregg.py
+++ Meilensteine/Meilenstein_04/easteregg.py
@@ -2,9 +2,11 @@
 import os
 from flask_restful import Resource, Api
 from flask import Flask, jsonify
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 api = Api(app)
+csrf = CSRFProtect(app)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 class brew_coffee(Resource):
