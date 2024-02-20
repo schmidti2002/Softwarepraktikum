235c3ad0208c22d5ae15371270431965da4e8737
NociCreep <nico@unserefamily.de>
2024-02-20 10:09:41 +0100

    parseString() getestet

--- Meilensteine/Meilenstein_04/frontend/test/InputView.test.js
+++ Meilensteine/Meilenstein_04/frontend/test/InputView.test.js
@@ -52,4 +52,12 @@ describe('InputView.js', () => {
     ])('parseIntArray()', async (input, solution) => {
         expect(InputView.parseIntArray(input)).toStrictEqual(solution);
     });
+
+    test.each([
+        ["0",{value: "0"}],
+        ["eins",{value: "eins"}],
+        ["zw31",{value: "zw31"}]
+    ])('parseString()', async (input, solution) => {
+        expect(InputView.parseString(input)).toStrictEqual(solution);
+    });
 });
