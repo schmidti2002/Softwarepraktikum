04e4d4cbfca0d09c0db96d4c916da88fa889e410
NociCreep <nico@unserefamily.de>
2024-02-20 10:06:36 +0100

    parseIntArray() getestet

--- Meilensteine/Meilenstein_04/frontend/test/InputView.test.js
+++ Meilensteine/Meilenstein_04/frontend/test/InputView.test.js
@@ -29,13 +29,27 @@ describe('InputView.js', () => {
         ["-1",{value: -1}],
         ["1024",{value: 1024}],
         ["-1024",{value: -1024}],
-        ["&0",{error: 'Darf nur eine Ganzzahl sein'}],
-        ["1.5",{error: 'Darf nur eine Ganzzahl sein'}],
-        ["+2,7",{error: 'Darf nur eine Ganzzahl sein'}],
-        ["8hp!",{error: 'Darf nur eine Ganzzahl sein'}],
-        ["Fünf",{error: 'Darf nur eine Ganzzahl sein'}]
+        ["&0", {error: 'Darf nur eine Ganzzahl sein'}],
+        ["1.5", {error: 'Darf nur eine Ganzzahl sein'}],
+        ["+2,7", {error: 'Darf nur eine Ganzzahl sein'}],
+        ["8hp!", {error: 'Darf nur eine Ganzzahl sein'}],
+        ["Fünf", {error: 'Darf nur eine Ganzzahl sein'}]
     ])('parseInt()', async (input, solution) => {
         expect(InputView.parseInt(input)).toStrictEqual(solution);
     });
 
+    test.each([
+        ["0",{value: [0]}],
+        ["1,2",{value: [1,2]}],
+        ["-1,4,5",{value: [-1,4,5]}],
+        ["1024,1,2,3,4,5,6,7,8,9",{value: [1024,1,2,3,4,5,6,7,8,9]}],
+        ["-1024,-1024,-1024,-1024",{value: [-1024,-1024,-1024,-1024]}],
+        ["&0",{error: `${'Darf nur eine Ganzzahl sein'} (Position ${0})`}],
+        ["0,1.5",{error: `${'Darf nur eine Ganzzahl sein'} (Position ${1})`}],
+        ["2,,7",{error: `${'Darf nur eine Ganzzahl sein'} (Position ${1})`}],
+        ["8hp!",{error: `${'Darf nur eine Ganzzahl sein'} (Position ${0})`}],
+        ["Fünf",{error: `${'Darf nur eine Ganzzahl sein'} (Position ${0})`}]
+    ])('parseIntArray()', async (input, solution) => {
+        expect(InputView.parseIntArray(input)).toStrictEqual(solution);
+    });
 });
