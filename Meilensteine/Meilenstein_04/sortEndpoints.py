2b99e3cb039042402aca1854d792cca922d31830
schmidti2002 <florian-hartha@web.de>
2024-02-22 18:06:39 +0100

    Tests, aber ich darf wahrscheinlich nochmal jede Request Anfarge umbauen

--- Meilensteine/Meilenstein_04/sortEndpoints.py
+++ Meilensteine/Meilenstein_04/sortEndpoints.py
@@ -5,6 +5,7 @@ from flask_restful import Api, Resource, abort
 from flask_cors import CORS
 import random
 import string
+import json
 
 # wichtige 
 from hashlib import sha256
@@ -14,7 +15,6 @@ from datetime import datetime
 import Endpoints_util
 
 app = Flask(__name__)
-CORS(app)
 api = Api(app)
 
 database = Endpoints_util.db_connect()
@@ -42,7 +42,7 @@ class sort_favorite(Resource):
             return abort(401, message="API key is missing or invalid")
         
         cursor = database.cursor()
-        cursor.execute("""SELECT id, name, data, state FROM public."SortFavorite" JOIN public."Sort" ON public."Sort".id = public."SortFavorite".data WHERE public."Sort".owner = %s;""", (user_uuid,))
+        cursor.execute("""SELECT public."SortFavorite".id, name, data, state FROM public."SortFavorite" JOIN public."Sort" ON public."Sort".id = public."SortFavorite".data WHERE public."Sort".owner = %s;""", (user_uuid,))
         result = cursor.fetchall()
         response_dic = []
         for i in range(len(result)):
@@ -69,10 +69,12 @@ class sort_favorite(Resource):
             cursor.execute("""INSERT INTO public."SortFavorite" (id, name, data, state) VALUES (%s, %s, %s, %s)""", (id, name, data, state))
             database.commit()
         except :
+            database.rollback()
             return abort(409, message="Send data conflicts with existing entry")
         
         return jsonify("new favorite created")
     
+class sort_favorite_id(Resource):
     def delete(self, favorite_id):
         user_uuid = Endpoints_util.getUserUUID(request, database)
         
@@ -115,18 +117,21 @@ class sort_data(Resource):
             return abort(401, message="API key is missing or invalid")
         
         try:
-            id = request.form.get("id")
-            values = request.form.get("values")
-            values = psycopg2.extras.Json([values])
+            data_resquest= json.loads(request.get_json())
+            id = data_resquest["id"]
+            values = data_resquest["values"]
+            type = data_resquest["type"]
             if id == None or values == None:
                 return abort(409, message="Send data conflicts with existing entry")
         except :
             return abort(409, message="Send data conflicts with existing entry")
-        
+    
         try:
-            cursor.execute("""INSERT INTO public."Sort" (id, values, owner) VALUES (%s, %s, %s)""", (id, values, user_uuid))
+            values =  "{" + ",".join(map(str, values)) + "}"
+            cursor.execute("""INSERT INTO public."Sort" (id, values, owner, type) VALUES (%s, %s, %s, %s)""", (id, values, user_uuid, type))
             database.commit()
         except :
+            database.rollback()
             return abort(409, message="Send data conflicts with existing entry")
         
         return jsonify("data entry created")
@@ -140,8 +145,16 @@ class sort_data_id(Resource):
         
         try:
             cursor.execute("""SELECT id, values, type FROM public."Sort" WHERE id = %s;""", (sort_id,))
+            result = cursor.fetchall()
+            if len(result) == 0:
+                return ("sort not found", 404)
         except :
             return abort(404, message="sort not found")
-        result = cursor.fetchall()
-        response_dic = {"id":result[0],"values": result[1], "type": result[2]}
+        response_dic = {"id":result[0][0],"values": result[0][1], "type": result[0][2]}
         return (response_dic,200)
+    
+api.add_resource(sort_algo, "/sort/algo")
+api.add_resource(sort_favorite, "/sort/favorite")
+api.add_resource(sort_favorite_id, "/sort/favorite/<string:favorite_id>")
+api.add_resource(sort_data, "/sort/data")
+api.add_resource(sort_data_id, "/sort/data/<string:sort_id>")
