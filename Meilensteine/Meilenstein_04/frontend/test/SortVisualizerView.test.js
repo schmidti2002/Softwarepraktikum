987aff41bcb1a11a099c05d03e431d8f7e9a4a2e
thori2t <thoralf.teumer@web.de>
2024-02-26 15:08:18 +0100

    Verbesserungen

--- Meilensteine/Meilenstein_04/frontend/test/SortVisualizerView.test.js
+++ Meilensteine/Meilenstein_04/frontend/test/SortVisualizerView.test.js
@@ -1,16 +1,30 @@
-import { describe, expect, it } from '@jest/globals';
+import {
+  describe, expect, it, beforeAll,
+} from '@jest/globals';
 import SortVisualizerView from '../src/SortVisualizerView';
 import SingletonManager from '../src/SingletonManager';
+import { mockFetchHtml } from './testUtils.test';
 
 describe('SortVisualizerView.test.js', () => {
-  // bringt den Fehler, dass fetch bei View.js nicht funktioniert, und man node-fetch nehmen soll.
-  // Ich wollte aber nicht in View.js rumwerkeln.
-  const sortVisualizerView = new SortVisualizerView(document.createElement('div'), (new SingletonManager()).get('EventReporter'));
-  sortVisualizerView.container = document.createElement('div');
+  mockFetchHtml('../src/SortVisualizerView.html');
+  let sortVisualizerView;
+  beforeAll(() => {
+    // Mock the HTML structure expected by the component
+    document.body.innerHTML = '<div id="sortview-container"></div>';
+
+    // Create an instance of SortVisualizerView
+    sortVisualizerView = new SortVisualizerView(document.createElement('div'), (new SingletonManager()).get('EventReporter'));
+
+    // Return the initPromise to ensure it resolves before tests
+    return sortVisualizerView.initPromise;
+  });
 
   it.each([
     [0, 1, 2, 3, 4, 5],
-    [50, 10, 42]])('renderData test', (data) => {
-    expect(sortVisualizerView.renderData(data)).toMatchSnapshot();
+    [50, 10, 42],
+    [1, 5, 13, 20],
+  ])('renderData test', (data) => {
+    sortVisualizerView.renderData(data);
+    expect(sortVisualizerView.container.innerHTML).toMatchSnapshot();
   });
 });
