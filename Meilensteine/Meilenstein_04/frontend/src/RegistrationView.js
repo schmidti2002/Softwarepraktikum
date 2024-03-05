1776c83ebfd2bc24c782a8e3fe7009739de7bb5d
CheesoC <mklol200@gmail.com>
2024-03-05 22:31:01 +0100

    Finalisierung Registration

--- Meilensteine/Meilenstein_04/frontend/src/RegistrationView.js
+++ Meilensteine/Meilenstein_04/frontend/src/RegistrationView.js
@@ -1,66 +1,68 @@
+import { v4 as uuidv4 } from 'uuid';
 import { UserApi } from './api/apis/UserApi.ts';
 import InputView from './InputView';
-import { notEmpty, validateEmail } from './inputValidators';
+import {
+  notEmpty, validateEmail, password, inputLength,
+} from './inputValidators';
 import View from './View';
 
 export default class RegistrationView extends View {
-    constructor(parentNode, singletonManager) {
-        const eventReporter = singletonManager.get('EventReporter');
-        super('RegistrationView', parentNode, eventReporter);
-        this.eventReporter = eventReporter;
+  #userApi;
 
-        this.initPromise.then(() => {
-            // Eingabemaske für Email, Nutzername und Passwort erzeugen
-            this.inputView = new InputView(
-                document.getElementById('registration'),
-                eventReporter,
-                () => {},
-            );
-            this.inputView.initPromise.then(() => this.inputView.loadConfig([
-                {
-                    name: 'Email',
-                    field: 'email',
-                    type: 'email',
-                    validators: [{ func: validateEmail }], 
-                },
-                {
-                    name: 'Benutzername',
-                    field: 'username',
-                    type: 'string',
-                    validators: [{ func: notEmpty }],
-                },
-                {
-                    name: 'Passwort',
-                    field: 'password',
-                    type: 'password',
-                    validators: [{ func: notEmpty }],
-                },
-            ]));
-        });
+  constructor(parentNode, singletonManager) {
+    const eventReporter = singletonManager.get('EventReporter');
+    super('RegistrationView', parentNode, eventReporter);
+    this.eventReporter = eventReporter;
+    this.#userApi = singletonManager.get('UserApi');
+    this.initPromise.then(() => {
+      // Eingabemaske für Email, Nutzername und Passwort erzeugen
+      this.inputView = new InputView(
+        document.getElementById('registration'),
+        eventReporter,
+        () => { },
+      );
+      this.inputView.initPromise.then(() => this.inputView.loadConfig([
+        {
+          name: 'Email',
+          field: 'email',
+          type: 'email',
+          validators: [{ func: validateEmail }],
+        },
+        {
+          name: 'Benutzername',
+          field: 'username',
+          type: 'string',
+          validators: [{ func: notEmpty }],
+        },
+        {
+          name: 'Passwort',
+          field: 'password',
+          type: 'password',
+          validators: [{
+            func: inputLength,
+            param: { min: 8 },
+          },
+          {
+            func: password,
+          },
+          ],
+        },
+      ]));
+    });
+  }
+
+  async onClickRegister() {
+    if (!this.inputView.validate()) {
+      this.eventReporter.info('Email, Nutzername und Passwort werden benötigt.');
+      return;
     }
 
-    async onClickRegister() {
-        if (!this.inputView.validate()) {
-            this.eventReporter.info('Email, Nutzername und Passwort werden benötigt.');
-            return;
-        }
+    const user = this.inputView.getValues(); // Werte aus der Eingabemaske abrufen
 
-        const { email, username, password } = this.inputView.getValues(); // Werte aus der Eingabemaske abrufen
-
-        try {
-            // Registrierung durchführen
-            // ToDo: Registierung für API implementieren
-            const registrationResponse = await UserApi.userPost({ email, username, password });  
-
-            if (registrationResponse.status === 200) { 
-                this.eventReporter.success('Registrierung erfolgreich. Sie können sich jetzt anmelden.');
-                // Weiterleiten zu Login-Seite 
-                window.location.href = 'LoginView.html'; 
-            } else {
-                this.eventReporter.error('Fehler bei der Registrierung.');
-            }
-        } catch (error) {
-            this.eventReporter.error('Fehler bei der API-Anfrage: ' + error.message);
-        }
-    }
+    // Registrierung durchführen
+    // ToDo: Registierung für API implementieren
+    this.#userApi.userPost({ userCreate: { ...user, admin: false, id: uuidv4() } }).then(() => {
+      window.location.reload();
+    }).catch(() => { this.eventReporter.error('Fehler bei der Registrierung.'); });
+  }
 }
